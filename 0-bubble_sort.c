72253698052167ee2d3d6ee1160685b0c54d252c
hectoriabandini <abuhmoseji@gmail.com>
2023-07-25 11:17:21 -0700

    swapp and sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#!/bin/bash
+#include <stdio.h>
+#include <stddef.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	for (size_t i = 0; i < size; i++ )
+		int swapped = 0;
+	{
+		for (size_t v = 0; v < size - i - 1; v++ )
+			if (array[j] > array[j+i])
+			{
+				swap(&array[j], &array[j+1]);
+				swapped = 1;
+			}
+	if (swapped = 0)
+	{
+		break;
+	}	
+	}
+
+}
---
+++ sort.h
@@ -0,0 +1,9 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void swap(int *a, int *b)
+
+
+#endif
