0d522f1943f0e97a08db873bdee62852d6b112ce
hectoriabandini <abuhmoseji@gmail.com>
2023-07-25 13:42:46 -0700

    nothing

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
+#ifndef SORT_H
+#define SORT_H
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
